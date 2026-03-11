abc66736c9c6dc11f66dc78081d7f88c9997998b
rayvidal1 <rayvidalrodrigues@gmail.com>
2026-03-11 18:44:27 -0300

    Bump asset and service worker versions to force PWA refresh

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "renovo-static-v4";
+﻿const CACHE_NAME = "renovo-static-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -72,3 +72,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
